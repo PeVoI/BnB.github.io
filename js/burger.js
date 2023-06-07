9153851341976a52f9582738a8dd64f62e71ef58
PeVoI <121054766+PeVoI@users.noreply.github.com>
2023-06-07 19:25:31 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,13 @@
+    const hamb = document.querySelector("#hamb");
+	const popup = document.querySelector("#popup");
+	const menu = document.querySelector("#menu").cloneNode(1);
+	
+	hamb.addEventListener("click", hambHandler);
+
+	function hambHandler(e) {
+		popup.classList.toggle('open');
+		renderPopup();
+	}
+	function renderPopup() {
+		popup.appendChild(menu);
+	}
